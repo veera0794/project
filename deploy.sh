c6317e34876564fe83dff705ae7e541fe5a77df7
veera0794 <premavjl@gmail.com>
2025-04-06 23:14:09 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-docker-compose up
+echo " Deployment Branch : $BRANCH_NAME"
+docker stop my-reactapp-container || true
+docker rm my-reactapp-container || true
+if [ "$BRANCH_NAME" = "dev" ] ; then
+    docker pull prema07/dev-react-img:latest
+    docker run -d -p 80:80 --name my-reactapp-container prema07/dev-react-img:latest
+elif [ "$BRANCH_NAME" = "main" ]; then
+    docker pull prema07/prod-react-img:latest
+    docker run -d -p 80:80 --name my-reactapp-container prema07/prod-react-img:latest
+fi
